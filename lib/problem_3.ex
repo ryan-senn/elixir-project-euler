c333f5955c6ec5fe6d268d8ed049dc42ba0f0965
ryan senn <ryan.senn.au@gmail.com>
2019-07-03 15:36:40 +1000

    getting started on problem 3, implement function to test if a number is prime

--- lib/problem_3.ex
+++ lib/problem_3.ex
@@ -1,11 +1,28 @@
 # Problem description: https://projecteuler.net/problem=3
 
 defmodule Problem3 do
+  require Integer
+
   def solution_1 do
-    # @todo, implement solution 1
+    2..13195
+    |> Enum.filter(fn i -> is_prime?(i, 2) end)
+    |> IO.inspect()
+
+    1
   end
 
-  # def solution_2 do
-  #   @todo, implement solution 2
-  # end
+  defp is_prime?(number, check) do
+    # reached the end without finding a division, it's prime
+    if(number == check) do
+      true
+    else
+      # found a division, bail out
+      if rem(number, check) == 0 do
+        false
+      else
+        # keep searching
+        is_prime?(number, check + 1)
+      end
+    end
+  end
 end
